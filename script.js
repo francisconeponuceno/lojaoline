f1ac5d6e304a67fd146314b18f593e4ad51e64aa
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-03-20 17:43:24 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const removeProductButons = document.getElementsByClassName('remove-product-button')
 
+
+
+const removeProductButons = document.getElementsByClassName('remove-product-button')
 for (var i = 0; i < removeProductButons.length; i++) {
-    removeProductButons[i].addEventListener('click',function(event) {
-        event.target.parentElement.parentElement.remove()
-    })
+    removeProductButons[i].addEventListener('click',removeProduct)
 }
     
 
-let totalAmount = 0
+function removeProduct(event) {
+    event.target.parentElement.parentElement.remove()
+    updateTotal()
+}
+
+function updateTotal() {
+    let totalAmount = 0
 const cartProducts = document.getElementsByClassName('cart-product')
 for (var i = 0; i < cartProducts.length; i++) {
     //console.log(cartProducts[i])
@@ -15,6 +21,6 @@ for (var i = 0; i < cartProducts.length; i++) {
     const productQuantity = cartProducts[i].getElementsByClassName('product-qtd-input')[0].value
     
     totalAmount += productPrice * productQuantity
-    
 }
-document.querySelector('.cart-total-container span').innerText = 'R$' + totalAmount.toFixed(2)
+document.querySelector('.cart-total-container span').innerText = 'R$' + totalAmount.toFixed(2).replace('.',',')
+}
