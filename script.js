361650aceba7e6767bd56d62041ea0b828383ea8
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-03-21 09:19:05 -0300

    atualizacao

--- script.js
+++ script.js
@@ -5,7 +5,48 @@ const removeProductButons = document.getElementsByClassName('remove-product-butt
 for (var i = 0; i < removeProductButons.length; i++) {
     removeProductButons[i].addEventListener('click',removeProduct)
 }
+
+
+const quantityInputs = document.getElementsByClassName('product-qtd-input')
+for (var i = 0; i < quantityInputs.length; i++) {
+    quantityInputs[i].addEventListener('change', updateTotal)
+}
+
+
+const addTocastButtons = document.getElementsByClassName('button-hover-background')
+for (var i = 0; i < addTocastButtons.length; i++) {
+    addTocastButtons[i].addEventListener('click',addProductTocart)
+}
+
+
+function addProductTocart(event) {
+    const button = event.target
+    const protuctInfos = button.parentElement.parentElement
+    const productImage = protuctInfos.getElementsByClassName('product-image')[0].src
+    const productTitle = protuctInfos.getElementsByClassName('product-title')[0].innerText
+    const productPrice = protuctInfos.getElementsByClassName('product-price')[0].innerText
     
+    let newCartProduct = document.createElement('tr')
+    newCartProduct.classList.add('cart-product')
+
+    newCartProduct.innerHTML = `
+    
+    <td class="product-identification">
+        <img src="${productImage}" alt="${productTitle}" class="cart-product-image">
+        <strong class="cart-product-title">${productTitle}</strong>
+    </td>
+    <td>
+        <span class="cart-product-price">${productPrice}</span>
+    </td>
+    <td>
+        <input type="number" value="1" min="0" class="product-qtd-input">
+        <button type="button" class="remove-product-button">Remover</button>
+    </td>
+    `
+
+    const tableBody = document.querySelector('.cart-table tbody')
+    tableBody.append(newCartProduct)
+}
 
 function removeProduct(event) {
     event.target.parentElement.parentElement.remove()
